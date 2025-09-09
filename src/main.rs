e262f3f3439adde408390445e733e7032c7b2e8d
kaydenireland <kaydenireland@aol.com>
2025-09-09 14:29:55 -0400

    loop & args

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,24 @@ fn main() {
     loop {
         // use the `>` character as the prompt
         // need to explicitly flush this to ensure it prints before read_line
-        print!("> ");
-        stdout().flush();
+        print!(">>> ");
+        let _ = stdout().flush();
 
         let mut input = String::new();
+
         stdin().read_line(&mut input).unwrap();
 
-        let command = input.trim();
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+
 
         let mut child = Command::new(command)
+            .args(args)
             .spawn()
             .unwrap();
 
         // don't accept another command until this one completes
-        child.wait();
+        let _ = child.wait();
     }
 }
