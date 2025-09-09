e40d4206506c519b9ff35bfaccdff4dde841618f
kaydenireland <kaydenireland@aol.com>
2025-09-09 14:10:48 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::io::{stdin, Write};
+use std::io::stdout;
+use std::process::Command;
+
+fn main() {
+
+    loop {
+        // use the `>` character as the prompt
+        // need to explicitly flush this to ensure it prints before read_line
+        print!("> ");
+        stdout().flush();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let command = input.trim();
+
+        let mut child = Command::new(command)
+            .spawn()
+            .unwrap();
+
+        // don't accept another command until this one completes
+        child.wait();
+    }
+}
